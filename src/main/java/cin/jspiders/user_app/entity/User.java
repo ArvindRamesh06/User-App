ec58c1cee632fe347b73733a6155001356c8049d
Arvind <arvindramesh.work@gmail.com>
2025-08-15 15:54:22 +0530

    User Entity class Updated

--- src/main/java/cin/jspiders/user_app/entity/User.java
+++ src/main/java/cin/jspiders/user_app/entity/User.java
@@ -1,5 +1,21 @@
 package cin.jspiders.user_app.entity;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private long contact;
+	private String location;
+	
 }
